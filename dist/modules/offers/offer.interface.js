12a029d4f4f834a6c3c64b5c6f61c4ae6b24d821
rafeulanamudoy <rafeuludoy@gmail.com>
2025-02-15 18:11:49 +0600

    dis folder remove

--- dist/modules/offers/offer.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AgreementType = void 0;
-var AgreementType;
-(function (AgreementType) {
-    AgreementType["FlatFee"] = "Flat_Fee";
-    AgreementType["HourlyFee"] = "Hourly_Fee";
-    AgreementType["Milestone"] = "Milestone";
-})(AgreementType || (exports.AgreementType = AgreementType = {}));
