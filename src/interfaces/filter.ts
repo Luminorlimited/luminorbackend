162d5c57638138f22f09cf4fa7726965e5565f2e
rafeulanamudoy <rafeuludoy@gmail.com>
2025-02-16 22:08:12 +0600

    filter for budget and project duration sync

--- src/interfaces/filter.ts
+++ src/interfaces/filter.ts
@@ -1,4 +1,12 @@
 export interface IFilters {
   query?: string;
   location?: string;
+  minBudget?: string;
+  maxBudget?: string;
+  projectMin?: string;
+  projectMax?: string;
+  industry?: string | string[];
+  skillType?: string | string[];
+  timeline?: string | string[];
+  [key: string]: any;
 }
