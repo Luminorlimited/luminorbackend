5eaa4008865468adda54a5dde339dae3f769ee25
rafeulanamudoy <rafeuludoy@gmail.com>
2025-02-17 19:23:47 +0600

    env file changed

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,8 @@ export const corsOptions = {
     // "https://tasneem-social-frontend.netlify.app",
     "https://luminoor.vercel.app",
      "http://localhost:3000",
-     "http://10.0.20.68:3000"
+     "http://10.0.20.68:3000",
+     "https://www.luminor-ltd.com"
      
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
