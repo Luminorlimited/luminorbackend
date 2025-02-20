c806fd76bfe17b69bebafc25ade5a7b40b8dd02c
rafeulanamudoy <rafeuludoy@gmail.com>
2025-02-20 11:08:05 +0600

    web

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,7 @@ export const corsOptions = {
 
 app.use(
   "/api/v1/stripe/payment-webhook",
+  ()=> (console.log("webhook")),
   express.raw({ type: "application/json" }),
   StripeController.handleWebHook
 );
