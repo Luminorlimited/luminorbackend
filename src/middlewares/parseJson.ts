16d24dea425c5164bd526fd5b5c0887e673c39bc
rafeulanamudoy <rafeuludoy@gmail.com>
2025-08-12 14:51:01 +0600

    pasejson sanatize

--- src/middlewares/parseJson.ts
+++ src/middlewares/parseJson.ts
@@ -6,12 +6,26 @@ export const parseBodyData = (
   next: NextFunction
 ): void => {
   try {
-
   
     if (req.body.data) {
-      req.body = JSON.parse(req.body.data); // Parse the nested JSON
+      req.body = JSON.parse(req.body.data);
     }
-    next(); // Proceed to the next middleware
+
+
+    for (const key in req.body) {
+      if (typeof req.body[key] === "string") {
+        const val = req.body[key].trim();
+        if ((val.startsWith("{") && val.endsWith("}")) || (val.startsWith("[") && val.endsWith("]")) || (val.startsWith('"') && val.endsWith('"'))) {
+          try {
+            req.body[key] = JSON.parse(val);
+          } catch {
+            
+          }
+        }
+      }
+    }
+
+    next();
   } catch (error: any) {
     console.error("Error parsing JSON:", error.message);
     res.status(400).json({
